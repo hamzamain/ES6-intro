dc4a2322bdc764efc80907601150f0e47bf13e53
hamzamain <mohammadmainuddin211@gmail.com>
2022-08-30 21:46:56 +0600

    another

--- map.js
+++ map.js
@@ -1,4 +1,5 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const doubleIt = num => num * 2;
 const doubledNumbers = numbers.map(doubleIt);
-console.log(doubledNumbers)
+console.log(doubledNumbers);
+const friends = [1, 2, 3, 4, 5, ,6 ,7 ,8];
