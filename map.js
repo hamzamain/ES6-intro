fb066d953e480159d999ca60759ded7cb9cf0ebd
hamzamain <mohammadmainuddin211@gmail.com>
2022-08-30 21:53:54 +0600

    third

--- map.js
+++ map.js
@@ -2,4 +2,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const doubleIt = num => num * 2;
 const doubledNumbers = numbers.map(doubleIt);
 console.log(doubledNumbers);
-const friends = [1, 2, 3, 4, 5, ,6 ,7 ,8];
+const tripleIt = num => num * 3;
+const friends = [1, 2, 3, 4, 5, 5,6 ,7 ,8];
+const tripled = friends.map(tripleIt);
+console.log(tripled)
